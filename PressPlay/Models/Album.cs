c1021ddefb8e1b7b0374eff30821b71a5b378eb3
Hakim Smith <hakim.smith19@yahoo.com>
2019-07-09 11:06:26 -0400

    added models and properties

---
+++ PressPlay/Models/Album.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PressPlay.Models
+{
+    public class Album
+    {
+        public int AlbumId { get; set; }
+        public string AlbumTitle { get; set; }
+        public string ImageUrl { get; set; }
+        public string RecordLabel { get; set; }
+
+        public int SongId { get; set; }
+
+        public Album(int albumId, string albumTitle, string imageUrl)
+        {
+            AlbumId = albumId;
+            AlbumTitle = albumTitle;
+            ImageUrl = imageUrl;
+        }
+
+        public Album()
+        {
+
+        }
+            
+    }
+}
