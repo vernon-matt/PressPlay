78a315feeaf5b0d810b7552dcd72873725c28391
Sergio Rodriguez <rsergio98@yahoo.com>
2019-07-11 10:12:47 -0400

    added models and their properties as well as constructors

---
+++ BackEnd/PressPlay/Models/Artist.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PressPlay.Models
+{
+    public class Artist
+    {
+        public int ArtistId { get; set; }
+        public string ArtistName { get; set; }
+        public string ImageUrl { get; set; }
+
+        public int AlbumId { get; set; }
+
+        public Artist(int artistId, string artistName, string imageUrl)
+        {
+            ArtistId = artistId;
+            ArtistName = artistName;
+            ImageUrl = imageUrl;
+        }
+
+        public Artist()
+        {
+
+        }
+    }
+}
---
+++ BackEnd/PressPlay/Models/UserInput.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PressPlay.Models
+{
+    public class UserInput
+    {
+        public int UserInputId { get; set; }
+        public int Rating { get; set; }
+        public string Review { get; set; }
+
+        public int AlbumId { get; set; }
+
+        public UserInput(int userInputId, int rating, string review)
+        {
+            UserInputId = userInputId;
+            Rating = rating;
+            Review = review;
+        }
+
+        public UserInput()
+        {
+
+        }
+    }
+}
